acd477d154413f7f9e3ce1d773ec55ef505d6d6f
Ivan Terziev <ivanterziev38@gmail.com>
2022-03-05 22:49:23 +0200

    Separating connection, adding sql query

--- connection.py
+++ connection.py
@@ -1,42 +1,20 @@
-import mysql.connector
-from mysql.connector import Error
+from query import Connection
 
-try:
-    # Info
-    host = 'localhost'
-    database = 'Airport'
-    user =  'root'
-    password = 'Terziev123'
+# Info
+host = '127.0.0.1'
+database = 'WBD'
+user =  'root'
+password = 'Terziev123'
 
-    # Connect
-    connection = mysql.connector.connect(host=host, database=database, user=user, password=password)
+# Create object
+connection = Connection(host, database, user, password)
+connection.startConnection()
 
-    if connection.is_connected():
-        
-        # Print the version of mqsql for testing the connection
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
+query = "SELECT * FROM Passenger LIMIT 3"
+records = connection.query(query)
+print("rows = " + str(len(records)) + "\n")
+for row in records:
+    print("ID = ", row[0])
+    print("Name = "+ str(row[1])+ "\n")
 
-        # Run a query
-        query = "SELECT * FROM Passenger"
-        cursor = connection.cursor()
-        cursor.execute(query)
-        records = cursor.fetchall()
-        print("Total number of rows: ", cursor.rowcount)
-
-        # Print the query results
-        print("\n Printing every row")
-        for row in records:
-            print("ID_psg = ",row[0])
-            print("Name = ", row[1])
-
-# Handle errors
-except Error as e:
-    print("Error while connecting to MySQL: ", e)
-
-# Close the connection
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed!")
+connection.stopConnection()
---
+++ query.py
@@ -0,0 +1,38 @@
+import mysql.connector
+from mysql.connector import Error
+
+class Connection:
+    db_conn = None
+    cursor = None
+    def __init__(self, ip, database, user, pas) -> None:
+        self.ip = ip
+        self.database = database 
+        self.user = user 
+        self.pas = pas
+        # self.connection = None
+
+    def startConnection(self):
+        try: 
+            self.db_conn = mysql.connector.connect(host=self.ip, database=self.database, user=self.user, password=self.pas)
+
+            if self.db_conn.is_connected():
+            
+                # Print the version of mqsql for testing the connection
+                db_info = self.db_conn.get_server_info()
+                print("Connected to MySQL Server version ", db_info, "\n")
+                
+        except Error as e:
+            print("Error while connecting to MySQL: ", e)
+
+    def query(self, query):
+        cursor = self.db_conn.cursor()
+        cursor.execute(query)
+        records = cursor.fetchall()
+        
+        cursor.close()
+        return records
+
+    def stopConnection(self):
+        if self.db_conn.is_connected():
+            self.db_conn.close()
+            print("MySQL connection is closed!")
