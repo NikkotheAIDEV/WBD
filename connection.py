37c50a231b3374288b914faea2709f8b76f989e2
Ivan Terziev <ivanterziev38@gmail.com>
2022-03-05 16:20:27 +0200

    Started woring on database connection

---
+++ connection.py
@@ -0,0 +1,40 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    host = 'localhost'
+    database = 'Airport'
+    user =  'root'
+    password = 'Terziev123'
+
+    connection = mysql.connector.connect(host=host, database=database, user=user, password=password)
+
+    if connection.is_connected():
+        
+        # Print the version of mqsql for testing the connection
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+
+        # Run a query
+        query = "SELECT * FROM Passenger"
+        cursor = connection.cursor()
+        cursor.execute(query)
+        records = cursor.fetchall()
+        print("Total number of rows: ", cursor.rowcount)
+
+        # Print the query results
+        print("\n Printing every row")
+        for row in records:
+            print("ID_psg = ",row[0])
+            print("Name = ", row[1])
+
+# Handle errors
+except Error as e:
+    print("Error while connecting to MySQL: ", e)
+
+# Close the connection
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed!")
