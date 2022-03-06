e889368eeacc102ec3ec99a22c3e3b4d7cbd82de
Ivan Terziev <ivanterziev38@gmail.com>
2022-03-06 19:14:32 +0200

    Started working on tensorflow implementation

--- connection.py
+++ connection.py
@@ -1,8 +1,10 @@
 from query import Connection
+import numpy as np
+import tf
 
 # Info
 host = '127.0.0.1'
-database = 'WBD'
+database = 'Airport'
 user =  'root'
 password = 'Terziev123'
 
@@ -12,9 +14,15 @@ connection.startConnection()
 
 query = "SELECT * FROM Passenger LIMIT 3"
 records = connection.query(query)
-print("rows = " + str(len(records)) + "\n")
-for row in records:
-    print("ID = ", row[0])
-    print("Name = "+ str(row[1])+ "\n")
+
+# print("rows = " + str(len(records)) + "\n")
+# for row in records:
+#     print("ID = ", row[0])
+#     print("Name = "+ str(row[1])+ "\n")
+
+
+arr = np.array(records)
+tf.receive_data(arr)
+tf.print_data()
 
 connection.stopConnection()
---
+++ tf.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+
+data = None
+
+def receive_data(input):
+    global data
+    data = input
+
