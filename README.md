b3541653e71233da4f7119d62bf913490f117557
Ivan Terziev <ivanterziev38@gmail.com>
2022-03-10 18:53:24 +0200

    Updated the readme a bit and almost finnished the target labeling script

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 
-# WBD
+# WBD - Welcome to Big Data
 
 This is a work in progress application/website for giving recommendations on museums to visit based on previously visited ones.
 
-The project is developed for an university competiotion.
+The project is developed for an university competition.
+
+The projects uses these libraries:
+    Tensorflow
+    Numpy
+    Pandas
+    MySQL Connector Python
--- target.py
+++ target.py
@@ -1,8 +1,9 @@
 # Have to get id and most liked category + interested in categories, then find suatable museums(6 based on favourite + rating sorted in 5.0->0.0, 4 based on interested_in + rating > 2.0)
-
 from cgi import test
+from multiprocessing.dummy import Array
 from query import Connection
 import numpy as np
+import random
 
 # Info
 host = '127.0.0.1'
@@ -44,13 +45,37 @@ for x in range(2000):
 
     # Find every entry for this person
     for y in range(2000):
-        if person_id == data_interested_in[y, 1]:
-
-            # Check for a repeat
-            if data_interested_in[y, 2] not in all_entries:
+        if person_id == data_interested_in[y, 1] and data_interested_in[y, 2] not in all_entries:
                 all_entries.append(data_interested_in[y, 2])
 
         # Save into the dictionary
         interested_in_dic[person_id] = all_entries
 
+# Get the 100 highest rated museums in favourite category and select 6
+def request_favourites(id) -> Array:
+    query_favourites = "SELECT id FROM Museums WHERE category_id = {} ORDER BY avg_rating DESC LIMIT 100".format(id)
+    records_favourite = connection.query(query_favourites)
+    data_favourites = np.array(records_favourite)
+    fav_arr = []
+
+    for x in range(6):
+        z = random.randrange(1, 101)
+        fav_arr.append(data_favourites[z, 0])
+    
+    return fav_arr
+
+# Get a 100 museums in interested_in category and select 4
+def request_interested_in(id) -> Array:
+    query_interested_in = "SELECT id FROM Museums WHERE category_id = {} AND avg_rating > 0.5 LIMIT 100".format(id)
+    records_interested_in = connection.query(query_interested_in)
+    data_interested_in = np.array(records_interested_in)
+    interested_in_arr = []
+
+    for x in range(4):
+        z = random.randrange(1, 101)
+        interested_in_arr.append(data_interested_in[z, 0])
+
+    return interested_in_arr
+
+# End the connection
 connection.stopConnection()
